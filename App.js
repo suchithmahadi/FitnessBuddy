c5fd028d208ed6375fbeaafe3591cf140aba2eda
cwong2015 <sirhc8002@gmail.com>
2018-02-04 12:30:35 -0500

    moved css component into external js file

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styles from './styles'
 
 export default class App extends React.Component {
   render() {
@@ -12,12 +13,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,10 @@
+import React, {StyleSheet} from 'react-native'
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+});
