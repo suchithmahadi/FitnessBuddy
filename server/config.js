527cab07bd3b473c92b5f8baf7afe08a23ab4211
cwong2015 <sirhc8002@gmail.com>
2018-02-05 01:48:32 -0500

    added more passport code and secret key folder

---
+++ server/config.js
@@ -0,0 +1,5 @@
+export const google = {
+    clientID: 'INSERT-CLIENT-ID-HERE',
+    clientSecret: 'INSERT-CLIENT-SECRET-HERE',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+  };
--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
+import passport from 'passport';
+import GoogleStrategy from 'passport-google-oauth20';
+import { google } from './config';
 
 var app = express();
 
-app.listen('8000');
+app.listen('8000', ()=>{
+    console.log("Running on port 8000");
+});
